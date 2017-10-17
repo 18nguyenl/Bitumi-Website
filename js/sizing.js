6035d1992849d96470c64946c5ca046cee847e98
Long Nguyen <dystraxus@gmail.com>
2017-10-17 17:16:48 -0500

    FINISH ADDING IN SIGIL ANIMATION

--- js/sizing.js
+++ js/sizing.js
@@ -1,3 +1,4 @@
+var sigilAnimTime = 5000;
 $(document).ready(function() {
     var sigilSpace = Snap("#container-sigil");
     // Snap.load("./img/vectors/sigil.svg", function(vector) {
@@ -7,9 +8,6 @@ $(document).ready(function() {
     sigilSpace.attr({
         filter: sigilGlow
     });
-
-    //Transform Syntax = Raphael.js Rotate
-    // sigilSpace.transform("r30, 0, 0");
     var rect1 = sigilSpace.select("#r1"),
     rect2 = sigilSpace.select("#r3"),
     rect3 = sigilSpace.select("#r2"),
@@ -19,69 +17,101 @@ $(document).ready(function() {
     circle2 = sigilSpace.select("#c2"), 
     circle3 = sigilSpace.select("#c3"), 
     circle4 = sigilSpace.select("#c4");
-
-    // rect1.transform("r-50 629.8 114.6");
+    
     var flip = false;
-    setInterval(function() {
-        setTimeout(function() {
+    animateMainSigil()
+    setInterval(animateMainSigil, sigilAnimTime);
+    function animateMainSigil() {
+        if (!flip) {
+            circle4.animate({
+                // transform: "t656.9 216.8"
+                cx: 848.2,
+                cy: 600.8
+            }, sigilAnimTime, mina.easeinout);
+            circle4.animate({r: 150}, sigilAnimTime / 2, mina.easeinout, function() {
+                circle4.animate({r: 108.2}, sigilAnimTime / 2, mina.easeinout);
+            });
+            rect1.animate({
+                x1: 848.2,
+                y1: 600.8
+            }, sigilAnimTime, mina.easeinout);
+            rect1.animate({"stroke-width": 130}, sigilAnimTime / 2, mina.easeinout, function() {
+                rect1.animate({"stroke-width": 100}, sigilAnimTime / 2, mina.easeinout);
+            });
+            rect2.animate({
+                x1: 848.2,
+                y1: 600.8
+            }, sigilAnimTime, mina.easeinout);
+            rect2.animate({"stroke-width": 130}, sigilAnimTime / 2, mina.easeinout, function() {
+                rect2.animate({"stroke-width": 100}, sigilAnimTime / 2, mina.easeinout);
+            });
+            circle3.animate({
+                cx: 191.3,
+                cy: 384
+            }, sigilAnimTime, mina.easeinout);
+            circle3.animate({r:80}, sigilAnimTime / 2, mina.easeinout, function() {
+                circle3.animate({r:108.2}, sigilAnimTime / 2, mina.easeinout);
+            });
+            rect3.animate({
+                x2: 191.3,
+                y2: 384
+            }, sigilAnimTime, mina.easeinout);
+            rect3.animate({"stroke-width": 70}, sigilAnimTime / 2, mina.easeinout, function() {
+                rect3.animate({"stroke-width": 100}, sigilAnimTime / 2, mina.easeinout);
+            });
+            rect4.animate({
+                x2: 191.3,
+                y2: 384
+            }, sigilAnimTime, mina.easeinout);
+            rect4.animate({"stroke-width": 70}, sigilAnimTime / 2, mina.easeinout, function() {
+                rect4.animate({"stroke-width": 100}, sigilAnimTime / 2, mina.easeinout);
+            });
+            flip = true;
+        } else if (flip) {
             circle4.animate({
                 cx: 191.3,
                 cy: 384
-            }, 1000, mina.easeinout());
+            }, sigilAnimTime, mina.easeinout);
+            circle4.animate({r:80}, sigilAnimTime / 2, mina.easeinout, function() {
+                circle4.animate({r:108.2}, sigilAnimTime / 2, mina.easeinout);
+            });
             rect1.animate({
                 x1: 191.3,
                 y1: 384
-            }, 1000, mina.easeinout());
+            }, sigilAnimTime, mina.easeinout);
+            rect1.animate({"stroke-width": 70}, sigilAnimTime / 2, mina.easeinout, function() {
+                rect1.animate({"stroke-width": 100}, sigilAnimTime / 2, mina.easeinout);
+            });
             rect2.animate({
-                x1: 409.7,
-                y1: 876.7
-            }, 1000, mina.easeinout());
+                x1: 191.3,
+                y1: 384
+            }, sigilAnimTime, mina.easeinout);
+            rect2.animate({"stroke-width": 70}, sigilAnimTime / 2, mina.easeinout, function() {
+                rect2.animate({"stroke-width": 100}, sigilAnimTime / 2, mina.easeinout);
+            });
             circle3.animate({
                 cx: 848.2,
                 cy: 600.8
-            }, 1000, mina.easeinout());
+            }, sigilAnimTime, mina.easeinout);
+            circle3.animate({r: 150}, sigilAnimTime / 2, mina.easeinout, function() {
+                circle3.animate({r: 108.2}, sigilAnimTime / 2, mina.easeinout);
+            });
             rect3.animate({
-                x2: 409.7,
-                y2: 876.7
-            }, 1000, mina.easeinout());
+                x2: 848.7,
+                y2: 600.8
+            }, sigilAnimTime, mina.easeinout);
+            rect3.animate({"stroke-width": 130}, sigilAnimTime / 2, mina.easeinout, function() {
+                rect3.animate({"stroke-width": 100}, sigilAnimTime / 2, mina.easeinout);
+            });
             rect4.animate({
                 x2: 848.2,
                 y2: 600.8
-            }, 1000, mina.easeinout());
-        }, 2000);
-        circle4.animate({
-            // transform: "t656.9 216.8"
-            cx: 848.2,
-            cy: 600.8
-        }, 1000, mina.easeinout());
-        rect1.animate({
-            x1: 848.2,
-            y1: 600.8
-        }, 1000, mina.easeinout());
-        rect2.animate({
-            x1: 848.2,
-            y1: 600.8
-        }, 1000, mina.easeinout());
-        circle3.animate({
-            cx: 191.3,
-            cy: 384
-        }, 1000, mina.easeinout());
-        rect3.animate({
-            x2: 191.3,
-            y2: 384
-        }, 1000, mina.easeinout());
-        rect4.animate({
-            x2: 191.3,
-            y2: 384
-        }, 1000, mina.easeinout());
-        // var cTmp1 = circle3;
-        // circle3 = circle4;
-        // circle4 = cTmp1;
-        // var rTmp1 = rect1;
-        // rect1 = rect3;
-        // rect3 = rTmp1;
-        // var rTmp2 = rect2;
-        // rect2 = rect4;
-        // rect4 = rTmp2;
-    });
+            }, sigilAnimTime, (mina.easeinout));
+            rect4.animate({"stroke-width": 130}, sigilAnimTime / 2, mina.easeinout, function() {
+                rect4.animate({"stroke-width": 100}, sigilAnimTime / 2, mina.easeinout);
+            });
+            flip = false;
+        }
+    }
 });
+
