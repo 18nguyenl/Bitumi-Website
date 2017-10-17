4241acfb613b2a7cadaf063ca4355074ef561390
Eldrojin <dystraxus@gmail.com>
2017-10-17 15:19:23 -0500

    Try add animation loop base

--- js/sizing.js
+++ js/sizing.js
@@ -15,12 +15,73 @@ $(document).ready(function() {
     rect3 = sigilSpace.select("#r2"),
     rect4 = sigilSpace.select("#r4");
 
-
     var circle1 = sigilSpace.select("#c1"), 
     circle2 = sigilSpace.select("#c2"), 
     circle3 = sigilSpace.select("#c3"), 
     circle4 = sigilSpace.select("#c4");
 
     // rect1.transform("r-50 629.8 114.6");
-    rect1.animate({transform: "r-56 629.8 114.6"}, 1000, mina.easeinout());
+    var flip = false;
+    setInterval(function() {
+        setTimeout(function() {
+            circle4.animate({
+                cx: 191.3,
+                cy: 384
+            }, 1000, mina.easeinout());
+            rect1.animate({
+                x1: 191.3,
+                y1: 384
+            }, 1000, mina.easeinout());
+            rect2.animate({
+                x1: 409.7,
+                y1: 876.7
+            }, 1000, mina.easeinout());
+            circle3.animate({
+                cx: 848.2,
+                cy: 600.8
+            }, 1000, mina.easeinout());
+            rect3.animate({
+                x2: 409.7,
+                y2: 876.7
+            }, 1000, mina.easeinout());
+            rect4.animate({
+                x2: 848.2,
+                y2: 600.8
+            }, 1000, mina.easeinout());
+        }, 2000);
+        circle4.animate({
+            // transform: "t656.9 216.8"
+            cx: 848.2,
+            cy: 600.8
+        }, 1000, mina.easeinout());
+        rect1.animate({
+            x1: 848.2,
+            y1: 600.8
+        }, 1000, mina.easeinout());
+        rect2.animate({
+            x1: 848.2,
+            y1: 600.8
+        }, 1000, mina.easeinout());
+        circle3.animate({
+            cx: 191.3,
+            cy: 384
+        }, 1000, mina.easeinout());
+        rect3.animate({
+            x2: 191.3,
+            y2: 384
+        }, 1000, mina.easeinout());
+        rect4.animate({
+            x2: 191.3,
+            y2: 384
+        }, 1000, mina.easeinout());
+        // var cTmp1 = circle3;
+        // circle3 = circle4;
+        // circle4 = cTmp1;
+        // var rTmp1 = rect1;
+        // rect1 = rect3;
+        // rect3 = rTmp1;
+        // var rTmp2 = rect2;
+        // rect2 = rect4;
+        // rect4 = rTmp2;
+    });
 });
