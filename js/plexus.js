b728667bf4f26495caa0f8e7f9d5d8e37cc0a042
Long Nguyen <dystraxus@gmail.com>
2018-03-08 20:43:19 -0600

    Fix Plexus area

--- js/plexus.js
+++ js/plexus.js
@@ -1,10 +1,10 @@
 particlesJS('plexus', {
   "particles": {
     "number": {
-      "value": 80,
+      "value": 100,
       "density": {
         "enable": true,
-        "value_area": 800
+        "value_area": 1000
       }
     },
     "color": {
@@ -26,8 +26,8 @@ particlesJS('plexus', {
       }
     },
     "opacity": {
-      "value": 0.5,
-      "random": false,
+      "value": 1,
+      "random": true,
       "anim": {
         "enable": false,
         "speed": 1,
@@ -36,32 +36,32 @@ particlesJS('plexus', {
       }
     },
     "size": {
-      "value": 4,
+      "value": 0,
       "random": true,
       "anim": {
         "enable": false,
-        "speed": 40,
+        "speed": 50,
         "size_min": 0.1,
         "sync": false
       }
     },
     "line_linked": {
       "enable": true,
-      "distance": 120,
+      "distance": 150,
       "color": "#27c4c8",
-      "opacity": 0.4,
-      "width": 1.5
+      "opacity": 2,
+      "width": 1.3
     },
     "move": {
       "enable": true,
-      "speed": 6,
+      "speed": 5,
       "direction": "none",
-      "random": false,
+      "random": true,
       "straight": false,
       "out_mode": "out",
       "bounce": false,
       "attract": {
-        "enable": false,
+        "enable": true,
         "rotateX": 600,
         "rotateY": 1200
       }
